c8f05277b8b21414e4ddca766f7310828426815e
buddekai <kaibudde@gmail.com>
2020-04-24 08:26:16 +0200

    started new function

---
+++ R/plotPrecipitation.R
@@ -0,0 +1,26 @@
+#' @title plotPrecipitation
+#' @description Plot the mean temperature data
+#' @details This function takes a data frame with daily temperature data for
+#' given station, calculates annual mean and plots it.
+#' @aliases plotwarmingstripes
+#' @aliases plotWarmingstripes
+#' @aliases plotwarmingStripes
+#' @author Kai Budde
+#' @export plotWarmingStripes
+#' @import lubridate
+#' @import dplyr
+#' @import ggplot2
+#' @import RColorBrewer
+#' @param df.temp A data frame
+#' @param startyear.mean A number
+#' @param endyear.mean A number
+#' @param style A character
+#' @param station.name a character
+
+# Created:     08/22/2019
+# Last edited: 08/22/2019
+
+plotPrecipitation <- function(df.temp, startyear.mean, endyear.mean,
+                              style = "continuous",
+                              station.name = NULL){
+}
